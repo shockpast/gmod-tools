ba8d0edec359bb1e30dfc54623d1a48aa65b69b4
shockpast <shockpast@gmail.com>
2022-07-08 01:20:39 +0300

    massive refactoring

--- srcds/generate_start_bat.py
+++ srcds/generate_start_bat.py
@@ -4,24 +4,14 @@ import platform
 import time
 import sys
 
-try:
-    import colorama
-except ImportError:
-    print("colorama not installed!")
-
+# pip install ...
 try:
     import cpuinfo
 except ImportError:
     print("cpuinfo not installed!")
 
-info_log    = f"{colorama.Fore.BLUE}[INFO]{colorama.Style.RESET_ALL}"
-input_log   = f"{colorama.Fore.LIGHTBLUE_EX}[INPUT]{colorama.Style.RESET_ALL}"
-warn_log    = f"{colorama.Fore.YELLOW}[WARN]{colorama.Style.RESET_ALL}"
-error_log   = f"{colorama.Fore.RED}[ERROR]{colorama.Style.RESET_ALL}"
-success_log = f"{colorama.Fore.GREEN}[SUCCESS]{colorama.Style.RESET_ALL}"
-wait_log    = f"{colorama.Fore.CYAN}[WAIT]{colorama.Style.RESET_ALL}"
-
-current_time = datetime.now().strftime("%H:%M:%S")
+# External.
+import print
 
 clear = lambda: os.system("cls")
 clear()
@@ -29,17 +19,20 @@ clear()
 """
     Port.
 """
-print(f"[{current_time}] | {info_log} Name: Port [*].")
-print(f"[{current_time}] | {info_log} Description: The port the server will run on.")
-print(f"[{current_time}] | {info_log} Arguments: integer (e.g. 27015)")
-port = int(input(f"[{current_time}] | {input_log} Value: "))
+print.prettyinfo("Name: Port [*].")
+print.prettyinfo("Description: The port the server will run on.")
+print.prettyinfo("Arguments: integer (e.g. 27015)")
+
+port = int(print.prettyinput("Value: "))
 
 if (not type(port) is int):
-    print(f"[{current_time}] | {error_log} server.port must be an integer!")
+    print.prettyerror("server.port must be an integer!")
+
     raise SystemExit
 
 elif (port <= 0 or port == ""):
-    print(f"[{current_time}] | {error_log} server.port must be something, like an example!")
+    print.prettyerror("server.port must be something, like an example!")
+
     raise SystemExit
 
 clear()
@@ -47,17 +40,20 @@ clear()
 """
     Tickrate.
 """
-print(f"[{current_time}] | {info_log} Name: Tick Rate [*].")
-print(f"[{current_time}] | {info_log} Description: The tick rate the server will run. Higher the number, more demanding it is on the CPU. A performant tickrate is around 30. Max: 128.")
-print(f"[{current_time}] | {info_log} Arguments: integer (e.g. 33)")
-tickrate = int(input(f"[{current_time}] | {input_log} Value: "))
+print.prettyinfo("Name: Tick Rate [*].")
+print.prettyinfo("Description: The tick rate the server will run. Higher the number, more demanding it is on the CPU. A performant tickrate is around 30. Max: 128.")
+print.prettyinfo("Arguments: integer (e.g. 33)")
+
+tickrate = int(print.prettyinput("Value: "))
 
 if (not type(tickrate) is int):
-    print(f"[{current_time}] | {error_log} server.tickrate must be an integer!")
+    print.prettyerror("server.tickrate must be an integer!")
+
     raise SystemExit
 
 elif (tickrate < 16 or tickrate > 128):
-    print(f"[{current_time}] | {error_log} server.tickrate can't be lower than 16 or higher than 128!")
+    print.prettyerror("server.tickrate can't be lower than 16 or higher than 128!")
+
     raise SystemExit
 
 clear()
@@ -65,22 +61,26 @@ clear()
 """
     LAN Server -> sv_lan 0/1.
 """
-print(f"[{current_time}] | {info_log} Name: LAN Server [*].")
-print(f"[{current_time}] | {info_log} Description: LAN servers only allow users from your local network to connect. Public servers will require you to port forward your server's port number.")
-print(f"[{current_time}] | {info_log} Arguments: integer (e.g. 0)")
-lan = int(input(f"[{current_time}] | {input_log} Value: "))
+print.prettyinfo("Name: LAN Server [*].")
+print.prettyinfo("Description: LAN servers only allow users from your local network to connect. Public servers will require you to port forward your server's port number.")
+print.prettyinfo("Arguments: integer (e.g. 0)")
+
+lan = int(print.prettyinput("Value: "))
 
 if (not type(lan) is int):
-    print(f"[{current_time}] | {error_log} server.lan must be an integer!")
+    print.prettyerror("server.lan must be an integer!")
+
     raise SystemExit
 
 elif (lan < 0 or lan > 1):
-    print(f"[{current_time}] | {error_log} server.lan can't be lower than 0 or higher than 1!")
+    print.prettyerror("server.lan can't be lower than 0 or higher than 1!")
+
     raise SystemExit
 
 elif (lan == 1):
-    print(f"[{current_time}] | {warn_log} server.lan = 1, it may not appear in your server browser!")
-    print(f"[{current_time}] | {info_log} Press [ENTER] to continue generation.")
+    print.prettywarn("server.lan = 1, it may not appear in your server browser!")
+    print.prettyinfo("Press [ENTER] to continue generation.")
+
     input("")
 
 clear()
@@ -88,17 +88,20 @@ clear()
 """
     Max Players.
 """
-print(f"[{current_time}] | {info_log} Name: Max Players [*].")
-print(f"[{current_time}] | {info_log} Description: The max amount of players that can play on this ")
-print(f"[{current_time}] | {info_log} Arguments: integer (e.g. 128)")
-max_players = int(input(f"[{current_time}] | {input_log} Value: "))
+print.prettyinfo("Name: Max Players [*].")
+print.prettyinfo("Description: The max amount of players that can play on this ")
+print.prettyinfo("Arguments: integer (e.g. 128)")
+
+max_players = int(print.prettyinput("Value: "))
 
 if (not type(max_players) is int):
-    print(f"[{current_time}] | {error_log} server.max_players must be an integer!")
+    print.prettyerror("server.max_players must be an integer!")
+
     raise SystemExit
 
 elif (max_players <= 0 or max_players > 128):
-    print(f"[{current_time}] | {error_log} server.max_players can't be lower than 1 or higher than 128!")
+    print.prettyerror("server.max_players can't be lower than 1 or higher than 128!")
+
     raise SystemExit
 
 clear()
@@ -106,17 +109,20 @@ clear()
 """
     Gamemode.
 """
-print(f"[{current_time}] | {info_log} Name: Gamemode [*].")
-print(f"[{current_time}] | {info_log} Description: The name of your gamemode's folder.")
-print(f"[{current_time}] | {info_log} Arguments: string (e.g. sandbox)")
-gamemode = str(input(f"[{current_time}] | {input_log} Value: "))
+print.prettyinfo("Name: Gamemode [*].")
+print.prettyinfo("Description: The name of your gamemode's folder.")
+print.prettyinfo("Arguments: string (e.g. sandbox)")
+
+gamemode = str(print.prettyinput("Value: "))
 
 if (not type(gamemode) is str):
-    print(f"[{current_time}] | {error_log} server.gamemode must be an string!")
+    print.prettyerror("server.gamemode must be an string!")
+
     raise SystemExit
 
 elif (gamemode == ""):
-    print(f"[{current_time}] | {error_log} server.gamemode can't be blank!")
+    print.prettyerror("server.gamemode can't be blank!")
+
     raise SystemExit
 
 clear()
@@ -124,17 +130,20 @@ clear()
 """
     Map.
 """
-print(f"[{current_time}] | {info_log} Name: Map [*].")
-print(f"[{current_time}] | {info_log} Description: The name of the map the server will run, must be included in your server files.")
-print(f"[{current_time}] | {info_log} Arguments: string (e.g. gm_construct)")
-map = str(input(f"[{current_time}] | {input_log} Value: "))
+print.prettyinfo("Name: Map [*].")
+print.prettyinfo("Description: The name of the map the server will run, must be included in your server files.")
+print.prettyinfo("Arguments: string (e.g. gm_construct)")
+
+map = str(print.prettyinput("Value: "))
 
 if (not type(map) is str):
-    print(f"[{current_time}] | {error_log} server.map must be an string!")
+    print.prettyerror("server.map must be an string!")
+
     raise SystemExit
 
 elif (map == ""):
-    print(f"[{current_time}] | {error_log} server.map can't be blank!")
+    print.prettyerror("server.map can't be blank!")
+
     raise SystemExit
 
 clear()
@@ -142,19 +151,20 @@ clear()
 """
     Workshop Collection ID.
 """
-print(f"[{current_time}] | {info_log} Name: Workshop Collection ID - skip.")
-print(f"[{current_time}] | {info_log} Description: It will skip adding of \"Workshop Collection ID\".")
-print(f"[{current_time}] | {info_log} Arguments: string")
-workshop_add = str(input(f"[{current_time}] | {input_log} Add (Y/N): "))
+print.prettyinfo("Name: Workshop Collection ID - skip.")
+print.prettyinfo("Description: It will skip adding of \"Workshop Collection ID\".")
+print.prettyinfo("Arguments: string")
+
+workshop_add = str(print.prettyinput("Add (Y/N): "))
 
 if (workshop_add == "Y"):
-    print(f"[{current_time}] | {info_log} Name: Workshop Collection ID - add.")
-    print(f"[{current_time}] | {info_log} Description: The ID your steam workshop collection. This will be the addons the server automatically downloads. (Clients will not download these files)")
-    print(f"[{current_time}] | {info_log} Arguments: integer (e.g. 2594826718)")
-    workshop = int(input(f"[{current_time}] | {input_log} Value: "))
+    print.prettyinfo("Name: Workshop Collection ID - add.")
+    print.prettyinfo("Description: The ID your steam workshop collection. This will be the addons the server automatically downloads. (Clients will not download these files)")
+    print.prettyinfo("Arguments: integer (e.g. 2594826718)")
+    workshop = int(print.prettyinput("Value: "))
 
     if (not type(workshop) is int):
-        print(f"[{current_time}] | {error_log} server.workshop must be an int!")
+        print.prettyerror("server.workshop must be an int!")
         raise SystemExit
 
     clear()
@@ -168,19 +178,19 @@ else:
     start.bat creating process.
 """
 try:
-    print(f"[{current_time}] | {wait_log} Creating \"start.bat\"... [1/4]")
+    print.prettywait("Creating \"start.bat\"... [1/4]")
 
     create_start_bat = open("start.bat", "x")
     time.sleep(0.2) 
 
-    print(f"[{current_time}] | {success_log} \"start.bat\" saved! [2/4]")
+    print.prettysuccess("\"start.bat\" saved! [2/4]")
 
 except FileExistsError:
-    print(f"[{current_time}] | {error_log} \"start.bat\" already exists!")
+    print.prettyerror("\"start.bat\" already exists!")
     raise SystemExit
 
 try:
-    print(f"[{current_time}] | {wait_log} Writing all configurations into \"start.bat\"... [3/4]")
+    print.prettywait("Writing all configurations into \"start.bat\"... [3/4]")
 
     port        = port
     tickrate    = tickrate
@@ -191,27 +201,27 @@ try:
     workshop    = workshop
 
     start_bat = open("start.bat", "a")
-    start_bat.write(f"start \"SRCDS\" /B srcds.exe -game garrysmod -conlog -port {port} -console -conclearlog -condebug -tvdisable -maxplayers {max_players} +gamemode {gamemode} +map {map} +host_workshop_collection \"{workshop}\" -tickrate {tickrate} +fps_max {tickrate} +sv_lan {lan}\n:: Created via Garry's Mod Tools at {current_time}!")
+    start_bat.write(f"start \"SRCDS\" /B srcds.exe -game garrysmod -conlog -port {port} -console -conclearlog -condebug -tvdisable -maxplayers {max_players} +gamemode {gamemode} +map {map} +host_workshop_collection \"{workshop}\" -tickrate {tickrate} +fps_max {tickrate} +sv_lan {lan}\n:: Created via Garry's Mod Tools!")
 
-    print(f"[{current_time}] | {success_log} \"start.bat\" successfully configured! [4/4]")
+    print.prettysuccess("\"start.bat\" successfully configured! [4/4]")
     raise SystemExit
 except Exception as unknown_error:
-    print(f"[{current_time}] | {warn_log} Whoops, unknown error!")
+    print.prettywarn("Whoops, unknown prettyerror!")
     # или пофикси сам
     # знаете, я и сам своего рода кодер
-    print(f"[{current_time}] | {warn_log} Contact with us: https://github.com/shockpast/gmod-tools/issues/new")
+    print.prettywarn("Contact with us: https://github.com/shockpast/gmod-tools/issues/new")
 
     file_time = datetime.now().strftime("%H.%M.%S")
 
     try:
-        create_error_log = open(f"error-{file_time}.log", "x")
+        create_error_log = open(f"prettyerror-{file_time}.log", "x")
         
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
 
-        error_log = open(f"error-{file_time}.log", "a")
+        error_log = open(f"prettyerror-{file_time}.log", "a")
         error_log.write(
-        f"""Whoops! That's an error!
+        f"""Whoops! That's an prettyerror!
         \n--------------------------------------------------\n
         Type: {exc_type}\n
         File: {fname}\n
@@ -225,6 +235,6 @@ except Exception as unknown_error:
 
         raise SystemExit
     except FileExistsError:
-        print(f"[{current_time}] | {error_log} \"error-{file_time}.log\" already exists!")
+        print.prettyerror(f"\"prettyerror-{file_time}.log\" already exists!")
 
         raise SystemExit
