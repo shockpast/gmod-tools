9c31a054b08761819177f4dbd7b9dd11edbb6418
shockpast <shockpast@gmail.com>
2022-07-08 01:49:44 +0300

    handler for errors

---
+++ srcds/pretty_error.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+import sys
+import platform
+import cpuinfo
+import os
+
+import pretty_print
+
+def prettyerror(error):
+    file_time = datetime.now().strftime("%H.%M.%S")
+
+    try:
+        create_error_log = open(f"error-{file_time}.log", "x")
+        
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+
+        error_log = open(f"error-{file_time}.log", "a")
+        error_log.write(
+        f"""--------------------------------------------------\n
+    Type: "{exc_type}"\n
+    File: "{fname}"\n
+    Line: "{exc_tb.tb_lineno}"
+        \n--------------------------------------------------\n
+    Detailed Log: {error}
+        \n--------------------------------------------------\n
+    OS: "{platform.platform()}"\n
+    CPU: "{cpuinfo.get_cpu_info()["brand_raw"]}"\n
+    Python: "{platform.python_version()}"
+        \n--------------------------------------------------""")
+
+        raise SystemExit
+    except FileExistsError:
+        pretty_print.prettyerror(f"\"error-{file_time}.log\" already exists!")
+
+        raise SystemExit
