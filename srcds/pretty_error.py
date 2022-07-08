a23f31b5c09f6044510db92aa1b34909721ac663
shockpast <shockpast@gmail.com>
2022-07-08 14:02:14 +0300

    unused variables

--- srcds/pretty_error.py
+++ srcds/pretty_error.py
@@ -11,9 +11,9 @@ def prettyerror(error):
     file_time = datetime.now().strftime("%H.%M.%S")
 
     try:
-        create_error_log = open(f"error-{file_time}.log", "x")
+        _ = open(f"error-{file_time}.log", "x")
         
-        exc_type, exc_obj, exc_tb = sys.exc_info()
+        exc_type, _, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
 
         error_log = open(f"error-{file_time}.log", "a")
